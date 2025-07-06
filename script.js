0f954b36b763382ca4154bb2d24cb75284ab1fa6
restmandayooo <sanxiusan8@gmail.com>
2025-07-06 10:44:11 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+import $ from "https://cdn.skypack.dev/jquery@3.6.0";
+import * as platform from "https://cdn.skypack.dev/platform@1.3.6";
+
+
+
+$("#delete").click(() => {
+  $("#notification").hide();
+});
+
+$("#page2").hide();
+$("#button").click(() => {
+  $("#page1").hide();
+  se.play();
+  $("#page2").show();
+});
+
+let c = 6 * 60 * 60;
+setInterval(() => {
+  $("#hour").text(Math.floor(c / 3600));
+  $("#min").text(Math.floor((c % 3600) / 60));
+  $("#sec").text(Math.floor(c % 60));
+  c = c - 1;
+}, 1000);
+$("#device").text(platform.description);
+$("#date").text(new Date().toLocaleString("ja-JP"));
